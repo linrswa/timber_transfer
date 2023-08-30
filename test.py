1174b173dd5f62ca8a8bd88cf53f9a6672f0dbf8
rswa <jimmy880108@gmail.com>
2023-08-30 11:38:06 +0800

    #FIX discriminators.py and test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 from dataset import NSynthDataset
 from torch.utils.data import DataLoader
 from components.encoders import SpeakerEncoder
+from components.discriminators import MulitiPeriodDiscriminator, MultiResolutionDiscriminator
 
 train_dataset = NSynthDataset(data_mode="test", sr=16000)
 
@@ -9,6 +10,6 @@ train_loader = DataLoader(train_dataset, batch_size=4, num_workers=4, shuffle=Tr
        
 fn, s, l, f = next(iter(train_loader)) 
 
-model = SpeakerEncoder()
+model = MulitiPeriodDiscriminator()
 
-mean_emb, convariance_emb = model(s)
+y_d_rs, y_d_gs, fmap_rs, fmap_gs = model(s.unsqueeze(dim=1), s.unsqueeze(dim=1))
