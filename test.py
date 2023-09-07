9943dc7f61c7bd3540951303e273da5074caeb5e
rswa <jimmy880108@gmail.com>
2023-09-07 18:23:04 +0800

    #ADD blocks for autoencoder.py *add MultiDimEmbHeader *add TCUB *fix Decoder to fit the new blocks

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 # %%
 from dataset import NSynthDataset
 from torch.utils.data import DataLoader
+import torch
 
 from components.encoders import TimbreEncoder
 from components.discriminators import MulitiPeriodDiscriminator, MultiResolutionDiscriminator
 from components.utils import discriminator_loss
-from components.ddsp_modify.autoencoder import Encoder
+from components.ddsp_modify.autoencoder import Encoder, Decoder
 
 
 train_dataset = NSynthDataset(data_mode="test", sr=16000)
@@ -14,10 +15,11 @@ train_loader = DataLoader(train_dataset, batch_size=4, num_workers=4, shuffle=Tr
        
 fn, s, l, f = next(iter(train_loader)) 
 
-model = Encoder()
-a = model(s, l, f)
-model = MulitiPeriodDiscriminator()
+encoder = Encoder()
+decoder = Decoder()
+encoder_out = encoder(s, l, f)
+decoder_out = decoder(encoder_out)
 #%%
-y_d_rs, y_d_gs, fmap_rs, fmap_gs = model(s.unsqueeze(dim=1), s.unsqueeze(dim=1))
-
-loss, r_losses, g_losses = discriminator_loss(y_d_rs, y_d_gs)
+# model = MulitiPeriodDiscriminator()
+# y_d_rs, y_d_gs, fmap_rs, fmap_gs = model(s.unsqueeze(dim=1), s.unsqueeze(dim=1))
+# loss, r_losses, g_losses = discriminator_loss(y_d_rs, y_d_gs)
