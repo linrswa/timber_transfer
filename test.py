5caec6aef4b69b277a178e0e15c0392335c04d62
rswa <jimmy880108@gmail.com>
2023-09-11 16:16:43 +0800

    #ADD train.py utils.py config.json Need to find out why output is always nan

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 import torch
 
 from components.encoders import TimbreEncoder
-from components.discriminators import MulitiPeriodDiscriminator, MultiResolutionDiscriminator
+from components.discriminators import MultiPeriodDiscriminator, MultiResolutionDiscriminator
 from components.utils import discriminator_loss
 from components.ddsp_modify.autoencoder import Encoder, Decoder
 from components.ddsp_modify.ddsp import DDSP
@@ -19,6 +19,6 @@ fn, s, l, f = next(iter(train_loader))
 ddsp = DDSP()
 add, sub, rec = ddsp(s, l, f)
 #%%
-# model = MulitiPeriodDiscriminator()
-# y_d_rs, y_d_gs, fmap_rs, fmap_gs = model(s.unsqueeze(dim=1), s.unsqueeze(dim=1))
-# loss, r_losses, g_losses = discriminator_loss(y_d_rs, y_d_gs)
+model = MultiPeriodDiscriminator()
+y_d_rs, y_d_gs, fmap_rs, fmap_gs = model(s.unsqueeze(dim=1), s.unsqueeze(dim=1))
+loss, r_losses, g_losses = discriminator_loss(y_d_rs, y_d_gs)
