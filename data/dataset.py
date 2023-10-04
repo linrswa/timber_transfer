d329c4c95959f5d1b3eaf9b94871a076e2ecd3b6
rswa <jimmy880108@gmail.com>
2023-10-04 19:04:25 +0800

    #FIX these files to fit mfcc data

--- data/dataset.py
+++ data/dataset.py
@@ -17,9 +17,20 @@ class NSynthDataset(torch.utils.data.Dataset):
         signal_path = os.path.join(self.dir_path, "signal/*")
         self.audio_list = glob(signal_path)
         self.info_type = (
-            ("signal", "loudness", "frequency_c")
+            {
+                "signal": "signal",
+                "loudness": "loudness",
+                "frequency": "frequency_c",
+                "mfcc": "mfcc",
+            }
             if frequency_with_confidence
-            else ("signal", "loudness", "frequency")
+            else 
+            {
+                "signal": "signal",
+                "loudness": "loudness",
+                "frequency": "frequency",
+                "mfcc": "mfcc",
+            }
         )
 
     def __len__(self):
@@ -29,13 +40,16 @@ class NSynthDataset(torch.utils.data.Dataset):
         signal_path = self.audio_list[idx]
         file_name = signal_path.split("/")[-1][:-4]
         signal = np.load(
-            os.path.join(self.dir_path, f"{self.info_type[0]}/{file_name}.npy")
+            os.path.join(self.dir_path, f"{self.info_type['signal']}/{file_name}.npy")
         ).astype("float32")
         loudness = np.load(
-            os.path.join(self.dir_path, f"{self.info_type[1]}/{file_name}.npy")
+            os.path.join(self.dir_path, f"{self.info_type['loudness']}/{file_name}.npy")
         ).astype("float32")[..., :-1]
         frequency = np.load(
-            os.path.join(self.dir_path, f"{self.info_type[2]}/{file_name}.npy")
+            os.path.join(self.dir_path, f"{self.info_type['frequency']}/{file_name}.npy")
+        ).astype("float32")
+        mfcc = np.load(
+            os.path.join(self.dir_path, f"{self.info_type['mfcc']}/{file_name}.npy")
         ).astype("float32")
 
-        return (file_name, signal, loudness, frequency)
+        return (file_name, signal, loudness, frequency, mfcc)
