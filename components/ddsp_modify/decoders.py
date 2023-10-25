dc4dc3cc2acc77c45d490c768bdb536a7bc0257f
rswa <jimmy880108@gmail.com>
2023-10-25 17:28:41 +0800

    #FIRST try: add attention mechanism into tcub

--- components/ddsp_modify/decoders.py
+++ components/ddsp_modify/decoders.py
@@ -6,22 +6,40 @@ import torch.nn.functional as F
 class TCUB(nn.Module):
     def __init__(self, temporal, in_ch, out_ch):
         super().__init__()
-        self.conv_1x1_input = nn.Conv1d(temporal, temporal, kernel_size=1, stride=1, padding=0)
-        self.conv_1x1_condition = nn.Conv1d(temporal, temporal, kernel_size=1, stride=1, padding=0)
-        self.dense_upsample = nn.Linear(in_ch, out_ch)
-        self.conv_1x1_output = nn.Conv1d(temporal, temporal, kernel_size=1, stride=1, padding=0)
         
+        self.conv_1x1_input = nn.Conv1d(in_channels=in_ch, out_channels=temporal, kernel_size=1, stride=1, padding=0)
+        self.conv_1x1_condition = nn.Conv1d(in_channels=in_ch, out_channels=temporal, kernel_size=1, stride=1, padding=0)
+        
+        # Using PyTorch's MultiheadAttention
+        self.attention_block = nn.MultiheadAttention(embed_dim=in_ch, num_heads=8)
+        
+        self.fc_after_attention = nn.Linear(in_ch, out_ch)  # to transform the output to desired dimension
+        self.conv_1x1_output = nn.Conv1d(in_channels=2*temporal, out_channels=out_ch, kernel_size=1, stride=1, padding=0)
+
     def forward(self, x, condition):
-        x_input = self.conv_1x1_input(x)
-        x_condition = self.conv_1x1_condition(condition)
-        x_upsample = self.dense_upsample(x)
-        mix = torch.cat([x_input, x_condition], dim=-1)
+        x_input = self.conv_1x1_input(x.transpose(1, 2))
+        x_condition = self.conv_1x1_condition(condition.transpose(1, 2))
+        
+        # Applying attention
+        attn_output, _ = self.attention_block(x.transpose(0, 1), condition.transpose(0, 1), x.transpose(0, 1))
+        x_attention = self.fc_after_attention(attn_output).transpose(0, 1)
+        
+        mix = torch.cat([x_input, x_condition], dim=1)
         mix_tanh = torch.tanh(mix)
         mix_sigmoid = torch.sigmoid(mix)
         mix_output = mix_tanh * mix_sigmoid
         mix_output = self.conv_1x1_output(mix_output)
-        output = nn.LeakyReLU(0.2)(x_upsample + mix_output) 
+        
+        output = nn.LeakyReLU(0.2)(x_attention + mix_output.transpose(1, 2))
         return output 
+
+# Test the model
+model_attention = TCUB(temporal=250, in_ch=64, out_ch=128)
+x = torch.rand(5, 250, 64)  # batch, temporal, feature_dim
+condition = torch.rand(5, 250, 64)
+output_attention = model_attention(x, condition)
+output_attention.shape
+
 
 
 class Decoder(nn.Module):
@@ -41,7 +59,6 @@ class Decoder(nn.Module):
         self.tcrb_3 = TCUB(temporal=temporal, in_ch=256, out_ch=512)
     
         self.mlp_final = self.mlp(512 + 64, 512, mlp_layer) 
-        self.mlp_after_final = self.mlp(512, 512, mlp_layer) # new model test
         self.dense_harm = nn.Linear(512, n_harms + 1)
         self.dense_noise = nn.Linear(512, noise_filter_bank)
         
@@ -55,7 +72,6 @@ class Decoder(nn.Module):
         out_tcrb_3 = self.tcrb_3(out_tcrb_2, multi_timbre_emb[2].expand_as(out_tcrb_2).contiguous())
         out_cat_f0_loudness = torch.cat([out_tcrb_3, out_mlp_f0, out_mlp_loudness], dim=-1)
         out_mlp_final = self.mlp_final(out_cat_f0_loudness)
-        out_mlp_final = self.mlp_after_final(out_mlp_final) # new model test
         
         # harmonic part
         out_dense_harm = self.dense_harm(out_mlp_final)
