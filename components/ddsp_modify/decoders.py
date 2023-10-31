3ffd2ff08fa4d1aa5b86b2fb18e197ea3603440e
rswa <jimmy880108@gmail.com>
2023-10-31 15:28:09 +0800

    #FIX visual and add more figure to compare #Fix decoders more readable #Remove some unuse code in dataset.py

--- components/ddsp_modify/decoders.py
+++ components/ddsp_modify/decoders.py
@@ -4,25 +4,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TCUB(nn.Module):
-    def __init__(self, temporal, in_ch, out_ch):
+    def __init__(self, in_ch, out_ch):
         super().__init__()
         
-        self.conv_1x1_input = nn.Conv1d(in_channels=in_ch, out_channels=temporal, kernel_size=1, stride=1, padding=0)
-        self.conv_1x1_condition = nn.Conv1d(in_channels=in_ch, out_channels=temporal, kernel_size=1, stride=1, padding=0)
+        self.conv_1x1_input = nn.Conv1d(in_channels=in_ch, out_channels=in_ch, kernel_size=1)
+        self.conv_1x1_condition = nn.Conv1d(in_channels=in_ch, out_channels=in_ch, kernel_size=1)
         
         # Using PyTorch's MultiheadAttention
-        self.attention_block = nn.MultiheadAttention(embed_dim=in_ch, num_heads=8)
+        self.attention_block = nn.MultiheadAttention(embed_dim=in_ch, num_heads=8, batch_first=True)
         
         self.fc_after_attention = nn.Linear(in_ch, out_ch)  # to transform the output to desired dimension
-        self.conv_1x1_output = nn.Conv1d(in_channels=2*temporal, out_channels=out_ch, kernel_size=1, stride=1, padding=0)
+        self.conv_1x1_output = nn.Conv1d(in_channels=out_ch, out_channels=out_ch, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x, condition):
         x_input = self.conv_1x1_input(x.transpose(1, 2))
         x_condition = self.conv_1x1_condition(condition.transpose(1, 2))
         
         # Applying attention
-        attn_output, _ = self.attention_block(x.transpose(0, 1), condition.transpose(0, 1), x.transpose(0, 1))
-        x_attention = self.fc_after_attention(attn_output).transpose(0, 1)
+        attn_output, _ = self.attention_block(x, condition, x)
+        x_attention = self.fc_after_attention(attn_output)
         
         mix = torch.cat([x_input, x_condition], dim=1)
         mix_tanh = torch.tanh(mix)
@@ -32,14 +32,6 @@ class TCUB(nn.Module):
         
         output = nn.LeakyReLU(0.2)(x_attention + mix_output.transpose(1, 2))
         return output 
-
-# Test the model
-model_attention = TCUB(temporal=250, in_ch=64, out_ch=128)
-x = torch.rand(5, 250, 64)  # batch, temporal, feature_dim
-condition = torch.rand(5, 250, 64)
-output_attention = model_attention(x, condition)
-output_attention.shape
-
 
 
 class Decoder(nn.Module):
