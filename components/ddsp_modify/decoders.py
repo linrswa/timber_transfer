75234358aa597b4372c2a76be61ab4ba4d6c5ed2
linrswa <jimmy880108@gmail.com>
2023-12-06 13:52:54 +0800

    Refactor some code

--- components/ddsp_modify/decoders.py
+++ components/ddsp_modify/decoders.py
@@ -1,38 +1,7 @@
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-class TCUB(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super().__init__()
-        
-        self.conv_1x1_input = nn.Conv1d(in_channels=in_ch, out_channels=in_ch, kernel_size=1)
-        self.conv_1x1_condition = nn.Conv1d(in_channels=in_ch, out_channels=in_ch, kernel_size=1)
-        
-        # Using PyTorch's MultiheadAttention
-        self.attention_block = nn.MultiheadAttention(embed_dim=in_ch, num_heads=8, batch_first=True)
-        
-        self.fc_after_attention = nn.Linear(in_ch, out_ch)  # to transform the output to desired dimension
-        self.conv_1x1_output = nn.Conv1d(in_channels=out_ch, out_channels=out_ch, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x, condition):
-        x_input = self.conv_1x1_input(x.transpose(1, 2))
-        x_condition = self.conv_1x1_condition(condition.transpose(1, 2))
-        
-        # Applying attention
-        attn_output, _ = self.attention_block(x, condition, x)
-        x_attention = self.fc_after_attention(attn_output)
-        
-        mix = torch.cat([x_input, x_condition], dim=1)
-        mix_tanh = torch.tanh(mix)
-        mix_sigmoid = torch.sigmoid(mix)
-        mix_output = mix_tanh * mix_sigmoid
-        mix_output = self.conv_1x1_output(mix_output)
-        
-        output = nn.LeakyReLU(0.2)(x_attention + mix_output.transpose(1, 2))
-        return output 
-
+from .utils_blocks import TCUB
 
 class Decoder(nn.Module):
     def __init__(
@@ -73,7 +42,7 @@ class Decoder(nn.Module):
         global_amp = self.modified_sigmoid(out_dense_harm[..., :1])
         n_harm_amps = out_dense_harm[..., 1:]
         # n_harm_amps /= n_harm_amps.sum(-1, keepdim=True) # not every element >= 0
-        n_harm_amps_norm = F.softmax(n_harm_amps, dim=-1)
+        n_harm_amps_norm = nn.functional.softmax(n_harm_amps, dim=-1)
         harm_amp_distribution = global_amp * n_harm_amps_norm
 
         # noise filter part
