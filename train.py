441977786c286cd41c1bc270bb9be3fe43b9f048
rswa <jimmy880108@gmail.com>
2023-12-11 13:44:26 +0800

    Reduce model size

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ from utils import mel_spectrogram, get_hyparam, get_mean_std_dict, cal_loudness
 from data.dataset import NSynthDataset
 
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-run_name = "New_train_1"
-notes = "New model build from UpFusionBlock"
+run_name = "New_train_2"
+notes = "New model build from UpFusionBlock, reduce mlp layer to 3, reduce on UpFusionBLock"
 h = get_hyparam()
 
 def cal_mean_loss(total_mean_loss, batch_mean_loss, n_element):
