d450bd4cdbb91b116c995821085382b5a90a9b3d
rswa <jimmy880108@gmail.com>
2024-03-07 15:29:58 +0800

    Update pt_file paths and run_name

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from tools.loss_collector import LossCollector as L
 from data.dataset import NSynthDataset
 
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-run_name = "train1"
-notes = "new base of new project"
+run_name = "train2"
+notes = "change TCUB to TimbreAttFusionBlock."
 batch_size = 16
 
 h = get_hyparam()
