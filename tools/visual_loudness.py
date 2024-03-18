Fix loudness calculation

tools/visual_loudness.py
@@ -36,7 +36,7 @@ if USE_MEAN_STD:
     l_mod = cal_loudness_norm(l)
 
 ae = TimbreFusionAE().to("cpu")
-pt_file = f"{pt_file_dir}/train10_generator_best_40.pt"
+pt_file = f"{pt_file_dir}/train12_generator_best_0.pt"
 ae.load_state_dict(torch.load(f"{pt_file}", map_location="cpu"))
 
 synthsizer = HarmonicOscillator().to("cpu")

validation.py
@@ -1,4 +1,3 @@
-
 # %%
 import torch
 import torch.nn as nn
@@ -81,7 +80,8 @@ def valid_model(
     for val_p, val_s, val_l, val_f0_c in tqdm(valid_loader):
         val_s, val_l, val_f0_c = val_s.to(device), val_l.to(device), val_f0_c.to(device)
         val_f0, val_f0_confidence = seperate_f0_confidence(val_f0_c)
-        out_add, out_sub, out_rec, out_mu, out_logvar, global_amp = model(val_s, val_l, val_f0)
+        norm_l = cal_mean_std_loudness(val_l, mean_std_dict)
+        out_add, out_sub, out_rec, out_mu, out_logvar, global_amp = model(val_s, norm_l, val_f0)
 
         loss_l = get_loudness_l1_loss(out_rec, val_s, val_l, aw, mean_std_dict, norm=True)
         loss_f = get_pitch_l1_loss(out_rec, val_f0_c, device, cr, m_sec)
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     #     inquirer.List("pt_file", message="Choose a pt file", choices=pt_list_list)
     # }
     # pt_file = inquirer.prompt(pt_fonfirm)["pt_file"]
-    pt_file = "./pt_file/train10_generator_best_40.pt"
+    pt_file = "./pt_file/train12_generator_best_2.pt"
 
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     generator = TimbreTransformer(is_smooth=True, mlp_layer=3, n_harms=101).to(device)