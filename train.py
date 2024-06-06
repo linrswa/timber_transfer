7e43648d7b64df0a570230de713ecd516bb60e37
rswa <jimmy880108@gmail.com>
2024-06-06 19:23:36 +0800

    test decoder_v6

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from data.dataset import NSynthDataset
 
 #MARK: Train setting
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-run_name = "decoder_v6_1"
+run_name = "decoder_v6_2"
 notes = "new decoder"
 batch_size = 16
 
