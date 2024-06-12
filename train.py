676c6206e3f97c606c53bced57c9c098e5a7204f
rswa <jimmy880108@gmail.com>
2024-06-12 15:55:38 +0800

    train and valid decoder v8

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ from data.dataset import NSynthDataset
 
 #MARK: Train setting
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-run_name = "decoder_v7_1"
-notes = "new decoder"
+run_name = "decoder_v8_2"
+notes = "change extract size to 256"
 batch_size = 16
 
 h = get_hyparam()
