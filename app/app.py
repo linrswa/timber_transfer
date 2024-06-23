d121909e330409cab71b0f3a7eacf259bede2e2b
rswa <jimmy880108@gmail.com>
2024-06-23 23:19:28 +0800

    new decoder v18 implment enhance harmonic

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ from components.timbre_transformer.TimberTransformer import TimbreTransformer
 class GlobalInfo:
     def __init__(self):
         pt_dir = "../pt_file"
-        run_name = "decoder_v17_1(mfcc)"
+        run_name = "decoder_v18_2(mfcc)_addmfft"
         self.current_pt_file_name = f"{run_name}_generator_best_0.pt"
         self.pt_file = f"{pt_dir}/{self.current_pt_file_name}"
         self.pt_file_list = sorted(glob(f"{pt_dir}/{run_name}*.pt"))
