4d1f31baa19f8e093f56892e99d4e9f5a9000709
rswa <jimmy880108@gmail.com>
2024-07-01 12:03:29 +0800

    train and test decoder v19 and fix GRU init h to 0

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from data.dataset import NSynthDataset
 
 #MARK: Train setting
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-run_name = "decoder_v19_2_addmfft"
+run_name = "decoder_v19_3_addmfft"
 notes = "minor change MLP"
 batch_size = 16
 
