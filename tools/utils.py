d4665a0fbf2bed5b3f2a467b1da6e4d413f043f0
rswa <jimmy880108@gmail.com>
2024-07-11 08:43:24 +0800

    train deocoder v21 6 with mfftx3

--- tools/utils.py
+++ tools/utils.py
@@ -96,8 +96,8 @@ def get_hyparam():
     return h
 
 def cal_loudness_norm(l: ndarray):
-    mean_loudness = -20.22756776639393
-    std_loudness = 35.74932418725443
+    mean_loudness = -20.235496416593854
+    std_loudness = 36.28924789428713
     return (l - mean_loudness) / std_loudness
 
 # write a funcion make frequency transofrom to MIDI
